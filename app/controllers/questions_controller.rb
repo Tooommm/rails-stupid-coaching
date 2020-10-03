fe152c3139d7d80986cbc2df09e0d10c755e25c3
Thomas Brouillet <thomasbrouillet@gmail.com>
2020-10-03 12:18:36 +0200

    completed exercise

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,28 +3,16 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    @answer = params[:answer]
-    @reponse = coach_answer_enhanced(@answer)
-  end
-
-  def coach_answer(your_message)
-    length = your_message.length - 1
-    if your_message == 'I am going to work right now!'
-      ''
-    elsif your_message[length] == '?'
-      'Silly question, get dressed and go to work!'
+    @question = params[:question]
+    if @question.blank?
+      @answer = "I can't hear you!"
+    elsif @question =~ /i am going to work/i
+      @answer = 'Great!'
+    elsif @question.ends_with?('?')
+      @answer = 'Silly question, get dressed and go to work!'
     else
-      "I don't care, get dressed and go to work!"
+      @answer = "I don't care, get dressed and go to work!"
     end
   end
 
-  def coach_answer_enhanced(your_message)
-    if your_message == 'I AM GOING TO WORK RIGHT NOW!'
-      ''
-    elsif your_message == your_message.upcase
-      "I can feel your motivation! #{coach_answer(your_message)}"
-    else
-      coach_answer(your_message)
-    end
-  end
 end
